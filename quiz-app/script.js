d6d13562e6b66498c0481960f42627fe484d0c2a
priyajha <priyajha.tiim.18@gmail.com>
2022-08-02 17:38:00 +0530

    Project 1

---
+++ quiz-app/script.js
@@ -0,0 +1,26 @@
+const quizData = [
+    {
+        question: 'Which of the following is not a programming language?',
+        a: 'Python',
+        b: 'HTML',
+        c: 'JavaScript',
+        d: 'Java',
+        answer: 'b'
+    },
+    {
+        question: 'Which is the most used programming language of 2021?',
+        a: 'C++',
+        b: 'C#',
+        c: 'JavaScript',
+        d: 'Java',
+        answer: 'c'
+    },
+    {
+        question: 'Which of the following is not a programming language?',
+        a: 'Python',
+        b: 'HTML',
+        c: 'JavaScript',
+        d: 'Java',
+        answer: 'HTML'
+    },
+]
